bada924fa527f63a0b6d8b7c67efa1a87260b168
Oluwajoba <oluwajobabello@gmail.com>
2020-09-08 03:10:01 +0100

    refactor "handleSubmit" and others to handle seacrh parameter / Body.js

--- src/Body.js
+++ src/Body.js
@@ -15,6 +15,20 @@ import Nomination from "./Nomination";
 
 /* eslint-disable no-unused-expressions */
 
+const handleSubmit = (e, movieNominated, setMovieNominated, movie) => {
+  e.preventDefault();
+  setMovieNominated([
+    ...movieNominated,
+    {
+      movieID: movie.imdbID,
+      movieTitle: movie.Title,
+      moviePoster: movie.Poster,
+      movieYear: movie.Year,
+      movieGenre: movie.Genre,
+    },
+  ]);
+};
+
 const deleteNomination = (id, movieNominated, setMovieNominated) => {
   setMovieNominated(
     movieNominated.filter((mNominated2) => {
@@ -27,23 +41,33 @@ const deleteNomination = (id, movieNominated, setMovieNominated) => {
 const Body = (props) => {
   const [movieNominated, setMovieNominated] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMovieNominated([
-      ...movieNominated,
-      {
-        movieID: props.data.imdbID,
-        movieTitle: props.data.Title,
-        moviePoster: props.data.Poster,
-        movieYear: props.data.Year,
-        movieGenre: props.data.Genre,
-      },
-    ]);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setMovieNominated([
+  //     ...movieNominated,
+  //     {
+  //       // movieID: props.data.imdbID,
+  //       // movieTitle: props.data.Title,
+  //       // moviePoster: props.data.Poster,
+  //       // movieYear: props.data.Year,
+  //       // movieGenre: props.data.Genre,
+  //       movieID: movie.imdbID,
+  //       movieTitle: movie.Title,
+  //       moviePoster: movie.Poster,
+  //       movieYear: movie.Year,
+  //       movieGenre: movie.Genre,
+  //     },
+  //   ]);
+  // };
 
   const disableNominatedMovie = movieNominated.find((mNominated) => {
     return mNominated.movieID == props.data.imdbID;
   });
+  // {
+  //   props.data.Search.map((movie) => {
+  //     console.log("movie111111: ", movie);
+  //   });
+  // }
 
   // <Image src={src} size='tiny' verticalAlign='top' /> <span>Top Aligned</span>
   // <Divider />
@@ -55,65 +79,86 @@ const Body = (props) => {
   return (
     <>
       {console.log("props from body: ", props)}
-      {props.data ? (
-        <>
-          {movieNominated.length > 4 ? (
-            <div className="banner">
-              <p>Nomination Limit Reached!!! Thanks</p>
-            </div>
-          ) : (
-            ""
-          )}
-          <div className="content1">
-            <Image src={props.data.Poster} size="tiny" verticalAlign="top" />
-            {/* Title: {props.data.Title}, ID: {props.data.imdbID}, */}
-            <div className="content2">
-              <span>
-                {props.data.Title}
-                {props.data.Year}
-              </span>
-              <span>{props.data.Awards}</span>
-              <span>{props.data.imdbRating}</span>
-              <span>{props.data.Plot}</span>
-              {movieNominated.length > 4 ? (
-                <button disabled>Nominate</button>
-              ) : disableNominatedMovie ? (
-                <button disabled>Nominate</button>
-              ) : (
-                (console.log(
-                  "disableNominatedMovie1452: ",
-                  disableNominatedMovie
-                ),
-                (<button onClick={handleSubmit}>Nominate</button>))
-              )}
-            </div>
-            <div>
-              <Divider />
-            </div>
-          </div>
-          <>
-            <div className="content3">
-              {console.log("movieNominated: ", movieNominated)}
-              {movieNominated.map((mNominated) => {
-                return (
-                  <Nomination
-                    id={mNominated.movieID}
-                    title={mNominated.movieTitle}
-                    poster={mNominated.moviePoster}
-                    year={mNominated.movieYear}
-                    genre={mNominated.movieGenre}
-                    delete={(id) =>
-                      deleteNomination(id, movieNominated, setMovieNominated)
-                    }
-                  />
-                );
-              })}
-            </div>
-          </>
-        </>
-      ) : (
-        console.log("nothing")
-      )}
+      {props.data
+        ? props.data.Search.map((movie) => {
+            console.log("movie111111: ", movie);
+            console.log("movie111111: ", movie.Poster);
+            return (
+              <>
+                {movieNominated.length > 4 ? (
+                  <div className="banner">
+                    <p>Nomination Limit Reached!!! Thanks</p>
+                  </div>
+                ) : (
+                  ""
+                )}
+                <div className="content1">
+                  <Image src={movie.Poster} size="tiny" verticalAlign="top" />
+                  {/* Title: {movie.Title}, ID: {movie.imdbID}, */}
+                  <div className="content2">
+                    <span>
+                      {movie.Title}
+                      {movie.Year}
+                    </span>
+                    <span>{movie.Awards}</span>
+                    <span>{movie.imdbRating}</span>
+                    <span>{movie.Plot}</span>
+                    {/* {props.data.Search.map((movie) => {
+                console.log("movie111111: ", movie);
+              })} */}
+                    {movieNominated.length > 4 ? (
+                      <button disabled>Nominate</button>
+                    ) : disableNominatedMovie ? (
+                      <button disabled>Nominate</button>
+                    ) : (
+                      (console.log(
+                        "disableNominatedMovie1452: ",
+                        disableNominatedMovie
+                      ),
+                      (
+                        // (<button onClick={handleSubmit}>Nominate</button>))
+                        <button
+                          onClick={(e) =>
+                            handleSubmit(
+                              e,
+                              movieNominated,
+                              setMovieNominated,
+                              movie
+                            )
+                          }
+                        >
+                          Nominate
+                        </button>
+                      ))
+                    )}
+                  </div>
+                  <div>
+                    <Divider />
+                  </div>
+                </div>
+              </>
+            );
+          })
+        : console.log("nothing")}
+      <>
+        <div className="content3">
+          {console.log("movieNominated: ", movieNominated)}
+          {movieNominated.map((mNominated) => {
+            return (
+              <Nomination
+                id={mNominated.movieID}
+                title={mNominated.movieTitle}
+                poster={mNominated.moviePoster}
+                year={mNominated.movieYear}
+                genre={mNominated.movieGenre}
+                delete={(id) =>
+                  deleteNomination(id, movieNominated, setMovieNominated)
+                }
+              />
+            );
+          })}
+        </div>
+      </>
     </>
   );
 };
