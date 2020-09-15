a0e5ae726a85fdd0b4155380161875c713c0c498
Oluwajoba <oluwajobabello@gmail.com>
2020-09-15 04:02:09 +0100

    add file preprocessor / plugins/index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,10 +19,11 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   require("@cypress/code-coverage/task")(on, config);
-
+  on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
   // add other tasks to be registered here
 
   // IMPORTANT to return the config object
   // with the any changed environment variables
+
   return config;
 };
